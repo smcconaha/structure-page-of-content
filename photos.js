e1bcdad880d105e2774a800c909e10b057265e19
smcconaha <steverunner1987@gmail.com>
2022-09-02 13:31:39 +0000

    added js and html buttons on photos page

--- photos.js
+++ photos.js
@@ -1,3 +1,21 @@
-let newPhoto = document.createElement('img');
-newPhoto.src = images/[2];
-document.main.appendChild(newPhoto);
+let pic1 = 'images/favorite-1.jpg';
+let pic2 = 'images/favorite-2.jpg';
+let pic3 = 'images/favorite-3.jpg';
+let pic4 = 'images/favorite-4.jpg';
+let img = document.getElementById('pic');
+let button1 = document.getElementById('button1');
+let button2 = document.getElementById('button2');
+let button3 = document.getElementById('button3');
+let button4 = document.getElementById('button4');
+button1.addEventListener('click', () => {
+    img.src = pic1;
+});
+button2.addEventListener('click', () => {
+    img.src = pic2;
+});
+button3.addEventListener('click', () => {
+    img.src = pic3;
+});
+button4.addEventListener('click', () => {
+    img.src = pic4;
+});
